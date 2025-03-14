749eb410374c96cd2c95637491f971306c350df6
Andika Kurniawan <68938620+BedMan95@users.noreply.github.com>
2025-03-14 02:49:35 +0000

    fix

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -191,10 +191,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = {
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'Pernikahan Andika Kurniawan dan Rizki Dwi Safitri',
+            dates: `${formatDate('2025-04-09 10:00')}/${formatDate('2024-04-09 23:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'https://goo.gl/maps/ALZR6FJZU3kxVwN86',
+            location: 'https://maps.app.goo.gl/C5Zxz8mcTXxZdmft9',
             ctz: 'Asia/Jakarta',
         };
 
